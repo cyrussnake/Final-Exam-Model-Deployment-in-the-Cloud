157daef2c14126583b961316fbf0d044c223db68
cyrussnake <134576793+cyrussnake@users.noreply.github.com>
2023-05-25 13:01:47 +0800

    Classify different cars

---
+++ Code.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache_resource
+def load_model():
+  model=tf.keras.models.load_model('CPE 019 Final Exam: Model Deployment in the Cloud.hdf5')
+  return model
+model=load_model()
+st.write("""
+# Car Classifier"""
+)
+file=st.file_uploader("Choose a car photo from computer",type=["jpg","png"])
+
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(128,128)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Audi', 'Hyundai Creta', 'Mahindra Scorpio', 'Rolls Royce', 'Swift', 'Tata Safari', 'Toyota Innova']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
